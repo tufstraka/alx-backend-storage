c77484ddd562019e39fd2e33c9d3dc6aeaae841b
tufstraka <keithkadima@gmail.com>
2023-09-02 21:45:05 +0300

    fix typo

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,4 @@
--- script that creates a table users with follwing fields
+-- script that creates a table users with following fields
 -- id, email, name
 CREATE TABLE IF NOT EXISTS users (
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
